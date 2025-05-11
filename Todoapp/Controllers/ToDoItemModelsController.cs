e845da10b0ab899c3aee274ab2f93bef9b7cd68c
matthewcucio <cuciomatthew@gmail.com>
2025-05-11 17:22:51 +0800

    Add experimental deadline support

--- Todoapp/Controllers/ToDoItemModelsController.cs
+++ Todoapp/Controllers/ToDoItemModelsController.cs
@@ -54,7 +54,7 @@ namespace Todoapp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,IsCompleted")] ToDoItemModel toDoItemModel)
+        public async Task<IActionResult> Create([Bind("Id,Title,IsCompleted,Deadline")] ToDoItemModel toDoItemModel)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace Todoapp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,IsCompleted")] ToDoItemModel toDoItemModel)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,IsCompleted,Deadline")] ToDoItemModel toDoItemModel)
         {
             if (id != toDoItemModel.Id)
             {
