3aaa17585fc1d8ffb944eb18a0b1735cb3b9fc82
matthewcucio <cuciomatthew@gmail.com>
2025-05-11 20:44:21 +0800

    Replace IsCompleted with Status field and dropdown options

---
+++ Todoapp/Migrations/20250511122335_UpdateToStatus.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todoapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateToStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "ToDoItemModels");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "ToDoItemModels",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ToDoItemModels");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "ToDoItemModels",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
